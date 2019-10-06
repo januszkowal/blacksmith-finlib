537dc6bf69e6ae9b2a4b012bc94ba7dcd6662709
januszkowal <janusz.kowal@interia.pl>
2019-10-06 21:23:03 +0200

    Update WeekDaySetPolicy.java

--- src/main/java/org/blacksmith/finlib/calendar/policy/WeekDaySetPolicy.java
+++ src/main/java/org/blacksmith/finlib/calendar/policy/WeekDaySetPolicy.java
@@ -7,20 +7,17 @@ import org.blacksmith.finlib.calendar.policy.helper.StandardDateToPartConverters
 
 public class WeekDaySetPolicy extends HolidaySetProvider<DayOfWeek> {
 
-  public static final WeekDaySetPolicy SAT_SUN = new WeekDaySetPolicy(new DayOfWeek[]{
-    DayOfWeek.SATURDAY,DayOfWeek.SUNDAY});
+  public static final WeekDaySetPolicy SAT_SUN = new WeekDaySetPolicy(DayOfWeek.SATURDAY,DayOfWeek.SUNDAY);
 
-  public static final WeekDaySetPolicy FRI_SAT = new WeekDaySetPolicy(new DayOfWeek[]{
-      DayOfWeek.FRIDAY,DayOfWeek.SATURDAY});
+  public static final WeekDaySetPolicy FRI_SAT = new WeekDaySetPolicy(DayOfWeek.FRIDAY,DayOfWeek.SATURDAY);
 
-  public static final WeekDaySetPolicy THU_FRI = new WeekDaySetPolicy(new DayOfWeek[]{
-      DayOfWeek.THURSDAY,DayOfWeek.FRIDAY});
+  public static final WeekDaySetPolicy THU_FRI = new WeekDaySetPolicy(DayOfWeek.THURSDAY,DayOfWeek.FRIDAY);
 
-  public WeekDaySetPolicy(DayOfWeek[] weekendDays) {
+  public WeekDaySetPolicy(DayOfWeek...weekendDays) {
     super(StandardDateToPartConverters.WEEK_DAY,weekendDays);
   }
 
-  public WeekDaySetPolicy(int[] weekendDays) {
+  public WeekDaySetPolicy(int...weekendDays) {
     super(StandardDateToPartConverters.WEEK_DAY,Arrays.stream(weekendDays).boxed().map(DayOfWeek::of).collect(Collectors.toSet()));
   }
 
