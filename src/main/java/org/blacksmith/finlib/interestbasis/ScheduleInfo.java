2859f592313cb6d46ea923c54fab8805cd1e382e
Janusz Kowal <jkowalrz@gmail.com>
2020-06-22 17:17:41 +0200

    Remove amount, rate from schedule info

--- src/main/java/org/blacksmith/finlib/interestbasis/ScheduleInfo.java
+++ src/main/java/org/blacksmith/finlib/interestbasis/ScheduleInfo.java
@@ -6,23 +6,20 @@ import org.blacksmith.finlib.basic.Amount;
 import org.blacksmith.finlib.basic.Rate;
 import org.blacksmith.finlib.datetime.Frequency;
 import lombok.Builder;
-import lombok.Data;
 
 @Value
 @Builder
 public class ScheduleInfo {
-  public static ScheduleInfo SIMPLE_SCHEDULE_INFO1 = builder()
+  public static ScheduleInfo SIMPLE_SCHEDULE_INFO = builder()
       .isEndOfMonthConvention(true)
       .build();
 
   private LocalDate startDate;
-  private LocalDate maturityDate;
+  private LocalDate endDate;
   private LocalDate couponStartDate;
   private LocalDate couponEndDate;
   private boolean isEndOfMonthConvention;
   private Frequency couponFrequency;
-  private Rate startInterestRate;
-  private Amount notional;
 
   public Amount getNotional(LocalDate date) {
     return Amount.ZERO;
