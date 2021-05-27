6d361363a872d6cce4a916b60d6571ceb1dec51f
Janusz Kowal <jkowalrz@gmail.com>
2021-05-27 20:02:45 +0200

    Batch calculation

--- src/test/java/org/blacksmith/finlib/curves/CurveGenerationTest.java
+++ src/test/java/org/blacksmith/finlib/curves/CurveGenerationTest.java
@@ -28,6 +28,11 @@ public class CurveGenerationTest {
   }
 
   @Test
+  public void createCurveWithoutReflectionPoints2() {
+    exportCurve2(create365DayKnots2(), Path.of("data.csv"));
+  }
+
+  @Test
   public void shouldGenerateMinSizeCurve() {
     List<Knot> knots = new ArrayList();
     knots.add(Knot.of(0, 2.43d));//1D
@@ -48,6 +53,25 @@ public class CurveGenerationTest {
     knots.add(Knot.of(182, 4.38d));//6M
     knots.add(Knot.of(273, 4.47d));//6M
     knots.add(Knot.of(365, 4.52d));//1Y
+    return knots;
+  }
+
+  private List<Knot> create365DayKnots2() {
+    List<Knot> knots = new ArrayList();
+    knots.add(Knot.of(0, 2.43d));//1D
+    knots.add(Knot.of(1, 2.50d));//1D
+    knots.add(Knot.of(7, 3.07d));//1D
+    knots.add(Knot.of(14, 3.36d));//1W
+    knots.add(Knot.of(30, 3.71d));//2W
+    knots.add(Knot.of(90, 4.27d));//1M
+    knots.add(Knot.of(182, 4.38d));//6M
+    knots.add(Knot.of(273, 4.47d));//6M
+    knots.add(Knot.of(365, 4.52d));//1Y
+    knots.add(Knot.of(365*2, 4.72d));//1Y
+    knots.add(Knot.of(365*3, 4.72d));//1Y
+    knots.add(Knot.of(365*5, 4.62d));//1Y
+    knots.add(Knot.of(365*10, 4.32d));//1Y
+    knots.add(Knot.of(365*25, 4.22d));//1Y
     return knots;
   }
 
@@ -95,6 +119,26 @@ public class CurveGenerationTest {
           String.valueOf(valuesAkimaApacheCommons.get(i).getY()),
           String.valueOf(valuesLinearBlackSmith.get(i).getY()),
           String.valueOf(valuesLinearApacheCommons.get(i).getY()),
+          valuesAkimaBlackSmith.get(i).isKnot() ? String.valueOf(valuesAkimaBlackSmith.get(i).getY()) : ""))
+          .forEach(pw::println);
+      System.out.println("Saved to: " + path.toAbsolutePath());
+    } catch (Exception e) {
+    }
+  }
+
+  private void exportCurve2(List<Knot> knots, Path path) {
+    int maxValue = knots.stream().mapToInt(Knot::getX).max().getAsInt();
+    CurveInterpolatorFactory factory = new CurveInterpolatorFactory();
+    var akimaInterpolatorBlackSmith = factory.getFunction("AkimaSplineBlacksmith", knots);
+//    var akimaInterpolatorBlackSmith = factory.getYieldCurveFunction("AkimaSplineBlacksmith", knots);
+    var valuesAkimaBlackSmith = akimaInterpolatorBlackSmith.values(0, maxValue);
+//    var valuesAkimaBlackSmith = IntStream.rangeClosed(0, maxValue).boxed()
+//        .map(x -> akimaInterpolatorBlackSmith.value(x))
+//        .collect(Collectors.toList());
+    try (PrintWriter pw = new PrintWriter(path.toFile())) {
+      pw.println("x,funAkimaBlacksmith,knot");
+      IntStream.rangeClosed(0, maxValue).boxed().map(i -> convertToCSV(String.valueOf(i),
+          String.valueOf(valuesAkimaBlackSmith.get(i).getY()),
           valuesAkimaBlackSmith.get(i).isKnot() ? String.valueOf(valuesAkimaBlackSmith.get(i).getY()) : ""))
           .forEach(pw::println);
       System.out.println("Saved to: " + path.toAbsolutePath());
