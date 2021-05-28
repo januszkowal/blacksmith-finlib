4653e23af46b1d614732debf4e46c585923ff75c
Janusz Kowal <jkowalrz@gmail.com>
2021-05-28 18:29:49 +0200

    interest rates & fra

--- src/test/java/org/blacksmith/finlib/curve/dbstruct/YieldCurveService.java
+++ src/test/java/org/blacksmith/finlib/curve/dbstruct/YieldCurveService.java
@@ -11,19 +11,19 @@ public interface YieldCurveService {
 
   double getInterestRateValue(CurveRateId curveRateId);
 
-  default double getFra(CurveRateId curveRateId, LocalDate asOfDate, LocalDate d1, LocalDate d2) {
+  default double getFra(CurveRateId curveRateId, LocalDate d1, LocalDate d2) {
     int yearLength = getYearLength(curveRateId);
     double dcf1 = getDcfValue(curveRateId, d1);
     double dcf2 = getDcfValue(curveRateId, d2);
-    return RateUtils.getFra(asOfDate, d1, d2, dcf1, dcf2, yearLength);
+    return RateUtils.getFra(curveRateId.getAsOfDate(), d1, d2, dcf1, dcf2, yearLength);
   }
 
-  default double getFra(CurveRateId curveRateId, LocalDate asOfDate, int l1, int l2) {
+  default double getFra(CurveRateId curveRateId, int l1, int l2) {
     int yearLength = getYearLength(curveRateId);
-    LocalDate d1 = asOfDate.plusDays(l1);
-    LocalDate d2 = asOfDate.plusDays(l2);
+    LocalDate d1 = curveRateId.getAsOfDate().plusDays(l1);
+    LocalDate d2 = curveRateId.getAsOfDate().plusDays(l2);
     double dcf1 = getDcfValue(curveRateId, d1);
     double dcf2 = getDcfValue(curveRateId, d2);
-    return RateUtils.getFra(asOfDate, d1, d2, dcf1, dcf2, yearLength);
+    return RateUtils.getFra(curveRateId.getAsOfDate(), d1, d2, dcf1, dcf2, yearLength);
   }
 }
