ef15b31717e003a4afd250b9aaf16272ba96a460
Janusz Kowal <jkowalrz@gmail.com>
2021-05-28 18:24:31 +0200

    interest rates & fra

--- src/main/java/org/blacksmith/finlib/curve/CurveFunctionFactory.java
+++ src/main/java/org/blacksmith/finlib/curve/CurveFunctionFactory.java
@@ -1,22 +1,23 @@
-package org.blacksmith.finlib.curves;
+package org.blacksmith.finlib.curve;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.blacksmith.finlib.curves.algoritm.AkimaSplineInterpolator;
-import org.blacksmith.finlib.curves.algoritm.LinearInterpolator;
-import org.blacksmith.finlib.curves.algoritm.PolynomialFunction;
-import org.blacksmith.finlib.curves.types.Knot;
-import org.blacksmith.finlib.curves.types.Point2D;
+import org.blacksmith.finlib.curve.algoritm.AkimaSplineInterpolator;
+import org.blacksmith.finlib.curve.algoritm.AlgorithmType;
+import org.blacksmith.finlib.curve.algoritm.LinearInterpolator;
+import org.blacksmith.finlib.curve.algoritm.PolynomialFunction;
+import org.blacksmith.finlib.curve.types.Knot;
+import org.blacksmith.finlib.curve.types.Point2D;
 
 public class CurveFunctionFactory {
-  public PolynomialFunction getFunction(CurveType curveType, double[] xvals, double[] yvals) {
+  public PolynomialFunction getFunction(AlgorithmType curveType, double[] xvals, double[] yvals) {
     PolynomialFunction curveFunction = null;
-    if (curveType == CurveType.AKIMA_SPLINE_BLACKSMITH) {
+    if (curveType == AlgorithmType.AKIMA_SPLINE_BLACKSMITH) {
       curveFunction = new AkimaSplineInterpolator().interpolate(xvals, yvals);
-    } else if (curveType == CurveType.LINEAR_BLACKSMITH) {
+    } else if (curveType == AlgorithmType.LINEAR_BLACKSMITH) {
       curveFunction = new LinearInterpolator().interpolate(xvals, yvals);
-    } else if (curveType == CurveType.AKIMA_SPLINE_APACHE_COMMONS) {
+    } else if (curveType == AlgorithmType.AKIMA_SPLINE_APACHE_COMMONS) {
       var akimaSplineApacheCommonsFunction =
           new org.apache.commons.math3.analysis.interpolation.AkimaSplineInterpolator().interpolate(xvals, yvals);
       curveFunction = new PolynomialFunction() {
@@ -30,7 +31,7 @@ public class CurveFunctionFactory {
           return akimaSplineApacheCommonsFunction.getKnots();
         }
       };
-    } else if (curveType == CurveType.LINEAR_APACHE_COMMONS) {
+    } else if (curveType == AlgorithmType.LINEAR_APACHE_COMMONS) {
       var linearApacheCommonsInterpolatorFunction =
           new org.apache.commons.math3.analysis.interpolation.LinearInterpolator().interpolate(xvals, yvals);
       curveFunction = new PolynomialFunction() {
@@ -48,12 +49,13 @@ public class CurveFunctionFactory {
     return curveFunction;
   }
 
-  public CurveFunction getFunction(CurveType type, List<Knot> knots) {
-    List<Point2D> knotsPoints = knots.stream().map(knot -> Point2D.of(knot.getX(), knot.getY()))
-        .sorted(Point2D.comparatorByX())
+  public CurveFunction getFunction(AlgorithmType type, List<Knot> knots) {
+    List<Point2D> knotsPoints = knots.stream()
+        .sorted()
+        .map(knot -> Point2D.of(knot.getX(), knot.getY()))
         .collect(Collectors.toList());
     var xvals = knotsPoints.stream().mapToDouble(Point2D::getX).toArray();
     var yvals = knotsPoints.stream().mapToDouble(Point2D::getY).toArray();
-    return new YieldCurveFunction(getFunction(type, xvals, yvals));
+    return new CurveFunctionImpl(getFunction(type, xvals, yvals));
   }
 }
