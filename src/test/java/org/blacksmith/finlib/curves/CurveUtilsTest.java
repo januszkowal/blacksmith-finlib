0f3abe3b196d7474e32261b6ea4a8675fc5a9182
januszkowal <janusz.kowal@interia.pl>
2021-05-28 10:03:25 +0200

    Update CurveUtilsTest.java

--- src/test/java/org/blacksmith/finlib/curves/CurveUtilsTest.java
+++ src/test/java/org/blacksmith/finlib/curves/CurveUtilsTest.java
@@ -45,19 +45,4 @@ public class CurveUtilsTest {
     assertThat(AlgorithmUtils.getKnotIndex0(knotsX, 9d)).isEqualTo(2);
     assertThat(AlgorithmUtils.getKnotIndex0(knotsX, 9.1d)).isEqualTo(2);
   }
-
-  @Test
-  public void roundingTest() {
-//    assertThat(2d).isEqualTo(Math.floor(2.1d));
-//    assertThat(2d).isEqualTo(Math.floor(2d));
-    // next
-    assertThat(3d).isEqualTo(Math.ceil(2.5d));
-    assertThat(3d).isEqualTo(Math.ceil(2.1d));
-    assertThat(2d).isEqualTo(Math.ceil(2.0d));
-//    assertThat(3d).isEqualTo(Math.round(2.5d));
-//    assertThat(2d).isEqualTo(Math.round(2.2d));
-//    assertThat(2d).isEqualTo(Math.round(1.9d));
-    double[] knots = {0, 1.2, 3.5, 8.2};
-    System.out.println(Arrays.stream(knots).mapToInt(x -> (int)x).boxed().collect(Collectors.toList()));
-  }
 }
