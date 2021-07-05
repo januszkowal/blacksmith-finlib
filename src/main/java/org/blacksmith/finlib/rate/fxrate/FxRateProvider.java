f90029d4dfa1e39425fc384b54a4dff8ad387c9e
Janusz Kowal <jkowalrz@gmail.com>
2021-07-05 09:54:47 +0200

    Fx Rates - remove redundant classes

--- src/main/java/org/blacksmith/finlib/rate/fxrate/FxRateProvider.java
+++
@@ -1,6 +0,0 @@
-package org.blacksmith.finlib.rate.fxrate;
-
-import org.blacksmith.finlib.marketdata.MarketDataProvider;
-
-public interface FxRateProvider extends MarketDataProvider<FxRateId, FxRate3> {
-}
