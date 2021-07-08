9464e50fcc1add063db8ebb0786561327cf7d95a
Janusz Kowal <jkowalrz@gmail.com>
2021-07-08 09:18:08 +0200

    Fx Rates - remove redundant classes

--- src/main/java/org/blacksmith/finlib/math/analysis/interpolation/QuadraticSplineInterpolator.java
+++ src/main/java/org/blacksmith/finlib/math/analysis/interpolation/QuadraticSplineInterpolator.java
@@ -13,17 +13,18 @@ public class QuadraticSplineInterpolator extends AbstractPolynomialInterpolator 
     // Differences between knot points
     final double[] h = new double[n];
     final double[] b = new double[n];
+    final double[] u = new double[n];
+    final double[] v = new double[n + 1];
+    final double[] z = new double[n + 1];
+
     for (int i = 0; i < n; i++) {
       h[i] = xValues[i + 1] - xValues[i];
       b[i] = (yValues[i + 1] - yValues[i]) / h[i];
     }
 
-    final double u[] = new double[n];
-    final double v[] = new double[n + 1];
     u[0] = 0.0;
     v[0] = 0.0;
     v[n] = 0.0;
-
     double g = 0;
     for (int i = 1; i < n; i++) {
       g = 2.0 * (h[i] + h[i - 1]) - h[i - 1] * u[i - 1];
@@ -32,9 +33,7 @@ public class QuadraticSplineInterpolator extends AbstractPolynomialInterpolator 
           (h[i - 1] * h[i]) - h[i - 1] * v[i - 1]) / g;
     }
 
-    final double z[] = new double[n + 1];
-    z[n] = 0d;
-
+    z[n] = 0.9;
     for (int i = n - 1; i >= 0; i--) {
       z[i] = v[i] - u[i] * z[i + 1];
     }
@@ -44,9 +43,9 @@ public class QuadraticSplineInterpolator extends AbstractPolynomialInterpolator 
     for (int i = 0; i < n; i++) {
       hi = h[i];
       coeffa = yValues[i];
-      coeffb = -hi * (z[i + 1] + 2d * z[i]) / 3d + b[i];
+      coeffb = -hi * (z[i + 1] + 2.0 * z[i]) / 3.0 + b[i];
       coeffc = z[i];
-      coeffd = (z[i + 1] - z[i]) / (3.0d * hi);
+      coeffd = (z[i + 1] - z[i]) / (3.0 * hi);
       polynomials[i] = new PolynomialFunction(coeffa, coeffb, coeffc, coeffd);
     }
 
